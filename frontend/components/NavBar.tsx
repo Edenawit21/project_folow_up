d2e6270df4804cf4e48e687421c24353da067e62
Edenawit21 <edidinkayehudelesa@gmail.com>
2025-05-07 15:42:23 -0700

    some changes on the navabr

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -3,19 +3,11 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="bg-blue-600 text-white px-6 py-4 shadow-md">
+    <nav className="bg-white-600 text-black px-6 py-4 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-xl font-bold">Project Follow-Up</div>
+        <div className="text-xl font-bold">Protrack</div>
         <div className="space-x-4">
-          <Link href="/" className="hover:underline">
-            Home
-          </Link>
-          <Link href="/login" className="hover:underline">
-            Login
-          </Link>
-          <Link href="/signup" className="hover:underline">
-            Sign Up
-          </Link>
+          
         </div>
       </div>
     </nav>
