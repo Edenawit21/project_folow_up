d1a3a4843ada933b19f092e8f93e774000bc9bd5
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-05-18 03:54:21 +0300

    correct the import of the file

---
+++ frontend/utils/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+  name: string;
+  email: string;
+  avatar: string;
+  loggedIn: boolean;
+}
+
+const initialState: UserState = {
+  name: "",
+  email: "",
+  avatar: "",
+  loggedIn: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login(state, action: PayloadAction<UserState>) {
+      return { ...state, ...action.payload, loggedIn: true };
+    },
+    logout() {
+      return { ...initialState };
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
