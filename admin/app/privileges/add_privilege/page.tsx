8bf54503568686cb26f2c979c5769bc2ee86bab5
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-06-02 07:55:03 +0300

    add privilege

--- admin/app/privileges/add_privilege/page.tsx
+++ admin/app/privileges/add_privilege/page.tsx
@@ -1,15 +1,9 @@
+"use client";
 import AddPrivilege from "@/components/privileges/AddPrivilege";
-
-export default function AddPrivilegePage() {
-  const handleCreate = (data: any) => {
-    console.log("Privilege created:", data);
-    // Add logic to save or process privilege data
-  };
-
-  const handleClose = () => {
-    console.log("AddPrivilege closed");
-    // Add logic to navigate back or hide the form
-  };
-
-  return <AddPrivilege onCreate={handleCreate} onClose={handleClose} />;
+export default function AddPrivilegePage() { 
+  return(<AddPrivilege onCreate={function (data: { name: string; description: string; }): void {
+    throw new Error("Function not implemented.");
+  } } onClose={function (): void {
+    throw new Error("Function not implemented.");
+  } }/>)
 }
